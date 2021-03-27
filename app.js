e6d09fc624422a788815ae6cdaf20e29232a3ceb
alanltr <letrocqueralan@gmail.com>
2021-03-27 17:16:33 +0100

    TRAINING: ajout du model et CRUD pour Product

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const Thing = require('./models/Things');
+const Product = require('./models/Product');
 
 // Connexion avec la BDD
 mongoose.connect('mongodb+srv://alan:lVAGdrM2KUiFqOk9@firstnodeserver.c4lpm.mongodb.net/Node?retryWrites=true&w=majority',
@@ -22,6 +23,10 @@ app.use((req, res, next) => {
 });
 
 app.use(express.json());
+
+// ====================================================
+//       CRUD model Thing | endpoint /api/stuff
+// ====================================================
 
 /**
  * Méthode POST
@@ -80,6 +85,57 @@ app.get('/api/stuff', (req, res, next) => {
   Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
+});
+
+// ====================================================
+//                   ENTRAINEMENT 
+//
+//      CRUD model Thing | endpoint /api/products
+// ====================================================
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+// GET all
+ app.get('/api/products', (req, res, next) => {
+  Product.find()
+    .then(products => res.status(200).json(products))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// GET one
+ app.get('/api/products/:id', (req, res, next) => {
+  Product.findOne({ _id: req.params.id })
+    .then(product => res.status(200).json(product))
+    .catch(error => res.status(404).json({ error }));
+});
+
+// POST
+app.post('/api/products', (req, res, next) => {
+  delete req.body._id;
+  const product = new Product({
+    ...req.body
+  });
+
+  product.save()
+    .then(() => res.status(201).json({ message: 'Produit enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
+});
+
+// PUT
+app.put('/api/products/:id', (req, res, next) => {
+ Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+   .then(() => res.status(200).json({ message: 'Modified!'}))
+   .catch(error => res.status(400).json({ error }));
+});
+
+// DELETE
+app.delete('/api/products/:id', (req, res, next) => {
+ Product.deleteOne({ _id: req.params.id })
+   .then(() => res.status(200).json({ message: 'Deleted!'}))
+   .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;

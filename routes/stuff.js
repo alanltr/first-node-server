d3c707263de0b2fa354c80d51268c4ec5119a8f3
alanltr <letrocqueralan@gmail.com>
2021-03-27 21:31:36 +0100

    JWT : protection des routes concernées par le jwt

--- routes/stuff.js
+++ routes/stuff.js
@@ -2,11 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 const stuffCtrl = require('../controllers/stuff');
+const auth = require('../middleware/auth');
 
-router.post(      '/',  stuffCtrl.createThing);
-router.put(    '/:id',  stuffCtrl.modifyThing);
-router.delete( '/:id',  stuffCtrl.deleteThing);
-router.get(    '/:id',  stuffCtrl.getOneThing);
-router.get(       '/',  stuffCtrl.getAllThing);
+// En ajoutant 'auth' avant les middleware, on oblige la requête a passer par ce middleware la avant, de sorte à checker le token
+router.post(      '/', auth,  stuffCtrl.createThing);
+router.put(    '/:id', auth,  stuffCtrl.modifyThing);
+router.delete( '/:id', auth,  stuffCtrl.deleteThing);
+router.get(    '/:id', auth,  stuffCtrl.getOneThing);
+router.get(       '/', auth,  stuffCtrl.getAllThing);
 
 module.exports = router;

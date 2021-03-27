034c4e2548a5d192ee9f8ed730cf945efe358cc7
alanltr <letrocqueralan@gmail.com>
2021-03-27 17:20:13 +0100

    Delete training | commit avant refacto architecture

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.json());
+app.use(bodyParser.json());
 
 // ====================================================
 //       CRUD model Thing | endpoint /api/stuff
@@ -85,57 +85,6 @@ app.get('/api/stuff', (req, res, next) => {
   Thing.find()
     .then(things => res.status(200).json(things))
     .catch(error => res.status(400).json({ error }));
-});
-
-// ====================================================
-//                   ENTRAINEMENT 
-//
-//      CRUD model Thing | endpoint /api/products
-// ====================================================
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
-// GET all
- app.get('/api/products', (req, res, next) => {
-  Product.find()
-    .then(products => res.status(200).json(products))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// GET one
- app.get('/api/products/:id', (req, res, next) => {
-  Product.findOne({ _id: req.params.id })
-    .then(product => res.status(200).json(product))
-    .catch(error => res.status(404).json({ error }));
-});
-
-// POST
-app.post('/api/products', (req, res, next) => {
-  delete req.body._id;
-  const product = new Product({
-    ...req.body
-  });
-
-  product.save()
-    .then(() => res.status(201).json({ message: 'Produit enregistré !' }))
-    .catch(error => res.status(400).json({ error }));
-});
-
-// PUT
-app.put('/api/products/:id', (req, res, next) => {
- Product.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
-   .then(() => res.status(200).json({ message: 'Modified!'}))
-   .catch(error => res.status(400).json({ error }));
-});
-
-// DELETE
-app.delete('/api/products/:id', (req, res, next) => {
- Product.deleteOne({ _id: req.params.id })
-   .then(() => res.status(200).json({ message: 'Deleted!'}))
-   .catch(error => res.status(400).json({ error }));
 });
 
 module.exports = app;
